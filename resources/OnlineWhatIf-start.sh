36a3d27361ceeef9c902772318c725727bfd5977
Jon Doig <z8290362@ad.unsw.edu.au>
2019-07-31 12:21:05 +1000

    Revert "Refer to tomcat8 not tomcat7"
    This reverts commit 644851cbee8139869b22bd1dd76a0aacdbb63468.

--- resources/OnlineWhatIf-start.sh
+++ resources/OnlineWhatIf-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-STAT=`sudo service tomcat8 status | grep pid`
+STAT=`sudo service tomcat7 status | grep pid`
 if [ -z "$STAT" ] ; then
-    sudo service tomcat8 start
+    sudo service tomcat7 start
     (sleep 2; echo "25"; sleep 2; echo "50"; sleep 2; echo "75"; sleep 2; echo "100") 	   | zenity --progress --auto-close --text "Online WhatIf starting"
 fi
 /usr/local/bin/geoserver_start.sh &
